72c15b1535770a8cc74b7c1f38063d36668cd87e
tanaya-077 <tanaya07khale@gmail.com>
2024-12-31 20:50:58 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+const btns = document.querySelectorAll(".row-btn");
+const display = document.querySelector("#dispaly");
+const clearBtn = document.querySelector("#clear-btn");
+
+btns.forEach((btn)=>{
+    btn.addEventListener("click",()=>{  
+            display.value += btn.value;
+    });
+})
+
+
+
+
+
+
+clearBtn.addEventListener("click", () => {
+    display.value = "";
+});
+
+
+const displayValue =()=>{
+    console.log(eval(display.value));
+    let displayVal = eval(display.value);
+    display.value += displayVal;
+  
+}
